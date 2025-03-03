ce09e3044d25595394a100b40d7ca2db8dfe13c9
Muhammad Ardhian Megatama <muhamad.ardian24@gmail.com>
2025-03-04 07:52:14 +0800

    Implement user information update with media handling; add transaction management and success/error responses

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Account\UpdateInformationRequest;
+use App\Http\Responses\InertiaFailedResponse;
+use App\Http\Responses\InertiaSuccessResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class AccountController extends Controller
@@ -15,6 +18,19 @@ class AccountController extends Controller
     public function updateInformation(UpdateInformationRequest $request)
     {
         $data = $request->validated();
-        dd($data);
+        DB::beginTransaction();
+        try{
+            $user = auth()->user();
+            $user->update($data);
+            if(isset($data['profilePicture'])){
+                $user->addMedia($data['profilePicture'])->toMediaCollection('profile_picture');
+            }
+            DB::commit();
+            return InertiaSuccessResponse::redirectBack('Successfully updated your information');
+        }catch(\Exception $e){
+            dd($e);
+            DB::rollBack();
+            return InertiaFailedResponse::redirectBack('Failed to update your information');
+        }
     }
 }
