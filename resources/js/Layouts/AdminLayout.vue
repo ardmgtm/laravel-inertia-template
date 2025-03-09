3578f92171f8e71e01bd809fdc586d622096bbb6
Muhammad Ardhian Megatama <muhamad.ardian24@gmail.com>
2025-03-09 10:08:50 +0800

    Remove unused import from AdminLayout component to streamline code.

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import Sidemenu from './Components/Sidemenu/Sidemenu.vue';
 import Header from './Components/Header/Header.vue';
 import { MenuItem } from 'primevue/menuitem';
