5583f93dc489901283b431546b3d1b78432a2446
Muhammad Ardhian <muhamad.ardian24@gmail.com>
2025-03-10 09:16:52 +0800

    Refactor AppInputSearch and UserManageView components for improved code clarity and UI transitions

--- resources/js/Components/AppInputSearch.vue
+++ resources/js/Components/AppInputSearch.vue
@@ -7,7 +7,7 @@
     </IconField>
 </template>
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, watch } from 'vue';
+import { defineEmits, ref, watch } from 'vue';
 
 const props = defineProps({
     modelValue: {
